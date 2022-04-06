9e25b0fcf9a2cfd0806bb97213455b4793e80e79
Lammertyn Pieter-Jan <pjlammertyn@gmail.com>
2022-04-06 20:58:07 +0000

    added lissing __init__.py

--- lib_airflow/operators/db/mssql.py
+++ lib_airflow/operators/db/mssql.py
@@ -1,11 +1,12 @@
 from typing import Sequence
 
-from airflow.providers.microsoft.mssql.operators.mssql import \
-    MsSqlOperator as BaseOperator
+from airflow.providers.microsoft.mssql.operators.mssql import (
+    MsSqlOperator as BaseOperator,
+)
 
 
 class MsSqlOperator(BaseOperator):
-    template_fields: Sequence[str] = ('sql', 'parameters')
+    template_fields: Sequence[str] = ("sql", "parameters")
 
     def __init__(
         self,
