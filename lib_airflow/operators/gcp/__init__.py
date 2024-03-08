c17dde1b3c6d328c0c96feb9cb9aaae9831db851
Lammertyn Pieter-Jan <pjlammertyn@gmail.com>
2024-03-08 04:54:24 +0000

    deprecated elasticsearch operator chzanged to pyproject.toml

--- lib_airflow/operators/gcp/__init__.py
+++ lib_airflow/operators/gcp/__init__.py
@@ -7,6 +7,6 @@ from .query_and_upload_to_bq import QueryAndUploadToBigQueryOperator  # isort:sk
 from .full_to_bq_base import FullUploadToBigQueryBaseOperator  # isort:skip
 
 from .bigquery_to_other_gcs import BigQueryToOtherGCSOperator
-from .elasticsearch_to_bq import ElasticSearchToBigQueryOperator
+#from .elasticsearch_to_bq import ElasticSearchToBigQueryOperator
 from .gcs_to_bigquery import GCSToBigQueryOperator
 from .paged_rest_to_bq import PagedRestToBigQueryOperator
